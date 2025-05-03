106a19a50fe1ef344c1bb9280715b3c6d9acdc4e
Harsh Patil <itsharshhh6@gmail.com>
2025-05-03 16:29:49 +0530

    git commit -"Product related classes added with DTO"

--- src/main/java/com/ecommerce/project/payload/ProductDTO.java
+++ src/main/java/com/ecommerce/project/payload/ProductDTO.java
@@ -1,26 +1,17 @@
-package com.ecommerce.project.model;
+package com.ecommerce.project.payload;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+@NoArgsConstructor
+public class ProductDTO {
     private Long productId;
     private String productName;
     private String description;
     private Integer quantity;
     private double price;
     private double specialPrice;
-
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Category category;
-
 }
---
+++ src/main/java/com/ecommerce/project/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.ecommerce.project.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository {
+}
