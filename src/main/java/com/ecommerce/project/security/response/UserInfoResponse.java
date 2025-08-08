55cad66d8a33012d5a9f6c4122eeee7ae9d24efb
Harsh Patil <itsharshhh6@gmail.com>
2025-08-08 15:34:03 +0530

    Signin endpoint

--- src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
+++ src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -1,14 +1,20 @@
-package com.ecommerce.project.security.jwt;
+package com.ecommerce.project.security.response;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-public class LoginResponse {
+public class UserInfoResponse {
+    @Getter
+    @Setter
+    private Long id;
     private String jwtToken;
-
     private String username;
     private List<String> roles;
 
-    public LoginResponse(String jwtToken, String username, List<String> roles) {
+    public UserInfoResponse(Long id, String jwtToken, String username, List<String> roles) {
+        this.id = id;
         this.jwtToken = jwtToken;
         this.username = username;
         this.roles = roles;
