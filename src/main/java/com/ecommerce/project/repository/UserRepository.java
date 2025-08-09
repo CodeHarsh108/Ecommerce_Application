ff4a03c856db3c3bce64d0c7abfc36c0cd7f9e48
Harsh Patil <itsharshhh6@gmail.com>
2025-08-09 08:56:14 +0530

    User-Repo updated

--- src/main/java/com/ecommerce/project/repository/UserRepository.java
+++ src/main/java/com/ecommerce/project/repository/UserRepository.java
@@ -1,10 +1,6 @@
 package com.ecommerce.project.repository;
 
 import com.ecommerce.project.model.User;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,9 +8,9 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUserName(String username);
+    Optional<User> findByUsername(String username);
 
-    boolean existsByUserName(@NotBlank @Max(value = 10, message = "Name can be maximum 10 characters long") @Min(value = 3, message = "Name must be minimum 3 characters long") String username);
+    boolean existsByUsername(String username);
 
-    boolean existsEmail(@NotBlank @Email  String email);
+    boolean existsByEmail(String email);
 }
