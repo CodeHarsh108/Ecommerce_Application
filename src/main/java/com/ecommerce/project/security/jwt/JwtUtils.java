dcbb9e89935335db0e58b51a7f59186243c78272
Harsh Patil <itsharshhh6@gmail.com>
2025-08-09 10:28:44 +0530

    Modifying code to implement Cookie Based Auth

--- src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -4,12 +4,15 @@ import com.ecommerce.project.security.service.UserDetailsImpl;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
@@ -26,16 +29,34 @@ public class JwtUtils {
     @Value("${spring.app.jwtSecret}")
     private String jwtSecret;
 
+    @Value("${spring.ecom.app.jwtCookieName}")
+    private String jwtCookie;
 
 
     // Getting Jwt from header
-    public String getJwtFromHeader(HttpServletRequest request){
-        String bearerToken = request.getHeader("Authorization");
-        logger.debug("Authorization Header : {}", bearerToken);
-        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
-            return bearerToken.substring(7);
+//    public String getJwtFromHeader(HttpServletRequest request){
+//        String bearerToken = request.getHeader("Authorization");
+//        logger.debug("Authorization Header : {}", bearerToken);
+//        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
+//            return bearerToken.substring(7);
+//        }
+//        return null;
+//    }
+
+    public String getJwtFromCookies(HttpServletRequest request){
+        Cookie cookie = WebUtils.getCookie(request, jwtCookie);
+        if(cookie != null){
+            System.out.println("COOKIE : " + cookie.getValue());
+            return cookie.getValue();
+        }else{
+            return null;
         }
-        return null;
+    }
+
+    public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal){
+        String jwt = generateTokenFromUsername(userPrincipal);
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie, jwt).path("/api").maxAge(24*60*60).httpOnly(false).build();
+        return cookie;
     }
 
 
