4cd6e325b005fe3a8bb9694c6c06421a23481620
Harsh Patil <itsharshhh6@gmail.com>
2025-08-10 14:42:38 +0530

    Add product to cart

---
+++ src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -0,0 +1,92 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.exceptions.APIException;
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
+import com.ecommerce.project.model.Cart;
+import com.ecommerce.project.model.CartItem;
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.CartDTO;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.repository.CartItemRepository;
+import com.ecommerce.project.repository.CartRepository;
+import com.ecommerce.project.repository.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CartServiceImpl implements CartService{
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    AuthUtil authUtil;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    CartItemRepository cartItemRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Override
+    public Object addProductToCart(Long productId, Integer quantity)  {
+        //Find existing cart or create one
+        Cart cart = createCart();
+        //Retrieve Product Details
+        Product product = productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
+        //Perform Validations
+        CartItem cartItem = cartItemRepository.findCartItemByProductIdAndCartId(cart.getCartId(), productId);
+        if(cartItem != null) {
+            return new APIException("Product " + product.getProductName() + "already exists in the cart");
+        }
+        if(product.getQuantity() == 0){
+            return new APIException("Product " + product.getProductName() + "is not available");
+        }
+        if(product.getQuantity() < quantity){
+            return new APIException("Please make an order of the : " + product.getProductName() + " less than or equal to the quantity " + product.getQuantity() + ".");
+        }
+        //Create Cart Item
+        CartItem newCartItem = new CartItem();
+        newCartItem.setProduct(product);
+        newCartItem.setCart(cart);
+        newCartItem.setQuantity(quantity);
+        newCartItem.setDiscount(product.getDiscount());
+        newCartItem.setProductPrice(product.getSpecialPrice());
+
+        //Save Cart Item
+        cartItemRepository.save(newCartItem);
+        product.setQuantity(product.getQuantity());
+        cart.setTotalPrice(cart.getTotalPrice() + (product.getSpecialPrice() * quantity ));
+        cartRepository.save(cart);
+
+        //Return the Updated Cart
+        CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+        List<CartItem> cartItems = cart.getCartItems();
+        Stream<ProductDTO> productDTOStream = cartItems.stream().map(item -> {
+            ProductDTO map = modelMapper.map(item.getProduct(), ProductDTO.class);
+            map.setQuantity(item.getQuantity());
+            return map;
+        });
+
+        cartDTO.setProducts(productDTOStream.toList());
+
+        return cartDTO;
+    }
+
+    private Cart createCart(){
+        Cart userCart = cartRepository.findCartByEmail(authUtil.loggedInEmail());
+        if(userCart != null){
+            return userCart;
+        }
+        Cart cart = new Cart();
+        cart.setTotalPrice(0.00);
+        cart.setUser(authUtil.loggedInUser);
+        Cart newCart = cartRepository.save(cart);
+        return newCart;
+    }
+}
