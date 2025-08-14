49ecbc2900b1a56ec006c1dc5290789652085247
Harsh Patil <itsharshhh6@gmail.com>
2025-08-14 08:34:11 +0530

    Order Controller

--- src/main/java/com/ecommerce/project/repository/PaymentRepository.java
+++ src/main/java/com/ecommerce/project/repository/PaymentRepository.java
@@ -4,7 +4,6 @@ import com.ecommerce.project.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.annotation.Repeatable;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
