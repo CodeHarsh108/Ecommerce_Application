608cf8d33a3ddfa3d390f8887e954f3e5b2935c1
Harsh Patil <itsharshhh6@gmail.com>
2025-08-14 08:14:38 +0530

    Payment DTO

--- src/main/java/com/ecommerce/project/payload/PaymentDTO.java
+++ src/main/java/com/ecommerce/project/payload/PaymentDTO.java
@@ -1,4 +1,17 @@
 package com.ecommerce.project.payload;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentDTO {
+    private Long paymentId;
+    private String paymentMethod;
+    private String pgPaymentId;
+    private String pgStatus;
+    private String pgResponseMessage;
+    private String pgName;
 }
