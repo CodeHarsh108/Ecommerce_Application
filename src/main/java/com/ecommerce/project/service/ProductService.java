9253f7f087e47a3dcbcdc4f7daf20a2fc9af607e
Harsh Patil <itsharshhh6@gmail.com>
2025-08-30 17:49:55 +0530

    Changes to be in accord with frontend

--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -19,6 +19,6 @@ public interface ProductService {
 
     ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException;
 
-    ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+    ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder, String keyword, String category);
 
 }
