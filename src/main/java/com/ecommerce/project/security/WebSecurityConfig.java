9d1b6188ba85b73a9fcaa6da2ce36261116db6d8
Harsh Patil <itsharshhh6@gmail.com>
2025-10-23 16:07:43 +0530

    Backend: WebSecurityConfig

--- src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -84,6 +85,8 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/public/**").permitAll()
                         .requestMatchers("/api/test/**").permitAll()
                         .requestMatchers("/images/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() //n//
+                        .requestMatchers("/api/seller/**").hasAnyRole("ADMIN", "SELLER") //n//
 
                         // Role-based access
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
